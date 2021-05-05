47a9fbb2a0b61a057389695307d899a54377f4e0
HolyPi <joanelly2018@gmail.com>
2021-05-05 15:09:59 -0400

    Added Todo list

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './Header/Header';
+import Store from './Store/Store';
+import Navbar2 from './Navbar2/Navbar2';
+import Navbar from './Navbar/Navbar';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <Router>
+     <div className="App">
+       
+     </div>
+   </Router>
   );
 }
 
